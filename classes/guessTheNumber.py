1ce586e06e19fd5037bc7356a08f9cd7969f6d6f
melissa-24 <melissalongenberger24@gmail.com>
2021-10-11 19:51:49 -0400

    added more to number game

--- classes/guessTheNumber.py
+++ classes/guessTheNumber.py
@@ -14,15 +14,27 @@ class GuessTheNumber:
     #     print(self.answer)
     
     def checkInput(self, choice):
-        if choice == self.answer:
-            message = print("You guessed correct!!\n")
-            message = print("Thank you for playing\nExiting to main screen\n\n")
-            self.leave = 1
-            return self.leave
-        else:
-            message = print("Sorry that was incorrect\n\n")
+        if choice > 25 or choice < 1:
+            message = print("That number is out of the range of possible answers.\n")
             self.leave = 0
             return self.leave
+        else:
+            if choice == self.answer:
+                message = print("You guessed correct!!\n")
+                message = print("Thank you for playing\nExiting to main screen\n\n")
+                self.leave = 1
+                return self.leave
+            if choice > self.answer:
+                message = print(f"Your guess of {choice} was too high try again")
+                self.leave = 0
+                return self.leave
+            if choice < self.answer:
+                message = print(f"Your guess of {choice} was too low try again")
+            else:
+                message = print("Sorry that was incorrect\n\n")
+                self.leave = 0
+                return self.leave
+
 
     def start(self):
         player = input("What shall we call you player?\n  ")
@@ -32,10 +44,31 @@ class GuessTheNumber:
         # self.print()
         game = True
         while game:
-            choice = input(f"\n\n{player}, please chose a number between 1-25\n  ")
-            choice = choice.split()
-            message = print(f"\nYou have chosen: {choice[0]}\nn")
-            self.checkInput(int(choice[0]))
-            if self.leave == 1:
+            levelChoice = input("Please chose the level you wish to play form the list by typing the coresponding number:\n\n1. Advanced speed round\n2. Basic\n\n")
+            levelChoice = levelChoice.split()
+            if levelChoice == '2':
+                choice = input(f"\n\n{player}, please chose a number between 1-25\n  ")
+                choice = choice.split()
+                message = print(f"\nYou have chosen: {choice[0]}\n")
+                self.checkInput(int(choice[0]))
+                if self.leave == 1:
+                    game = False
+            else:
+                message = print(f"\n{player} you have 3 tries to guess the right number\n")
+                round = [1,2,3]
+                for r in round:
+                    if r == 1 or 2:
+                        message = print(f"ROUND {r}")
+                        choice = input(f"\n\n{player}, please chose a number between 1-25\n  ")
+                        choice = choice.split()
+                        self.checkInput(int(choice[0]))
+                    else:
+                        message = print(f"ROUND {r}")
+                        choice = input(f"\n\n{player}, please chose a number between 1-25\n  ")
+                        choice = choice.split()
+                        self.checkInput(int(choice[0]))
+                        if self.leave == 1:
+                            game = False
+                message = print(f"Sorry you are out of turns")
+                self.leave = 1
                 game = False
-            
